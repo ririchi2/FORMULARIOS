59439098005fc47ae1e2a67c22a055a15e619ab0
Ricardo Pineda <richipineda2@gmail.com>
2021-02-17 18:02:02 +0000

    create project

---
+++ js/userNames.js
@@ -0,0 +1,21 @@
+const existingUserNames = [{
+    "userName": "Roby"
+}, {
+    "userName": "Roderigo"
+}, {
+    "userName": "Babbie"
+}, {
+    "userName": "Rozina"
+}, {
+    "userName": "Matthiew"
+}, {
+    "userName": "Flora"
+}, {
+    "userName": "Griffith"
+}, {
+    "userName": "Gregoire"
+}, {
+    "userName": "Nedi"
+}, {
+    "userName": "Hertha"
+}];
