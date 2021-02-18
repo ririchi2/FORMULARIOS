cf12064790cf92808a810faaa6a800de71685837
Ricardo Pineda <richipineda2@gmail.com>
2021-02-18 02:41:07 +0000

    form finished & variables assigned

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// Asignacion de variables para cada input
+var userName = document.getElementById("userName");
+var pwd = document.getElementById("pwd");
+var name = document.getElementById("name");
+var surname = document.getElementById("surname");
+var tlf = document.getElementById("tlf");
+var cp = document.getElementById("cp");
+var nie = document.getElementById("nie");
+var accType = document.getElementsByName("accType");
+var birthYear = document.getElementById("birthYear");
+var aficiones = document.getElementsByName("aficiones");
+var title = document.getElementById("title");
+var longdesc = document.getElementById("longdesc");
